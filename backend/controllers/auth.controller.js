e26d86339460d6aa2820f2f5aa491fdacdfe7ba4
Ayman ezz <aymanmae12@gmail.com>
2024-12-17 23:37:25 +0200

    first commit

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,12 @@
+export const signup =  async(req,res) => {
+  const {fullName, userName, password, confirmPassword, gender} = req.body
+  
+}
+export const login =  (req,res) => {
+  console.log("login controller is  Here!");
+  
+}
+export const logout =  (req,res) => {
+  console.log("logout controller is  Here!");
+  
+}
