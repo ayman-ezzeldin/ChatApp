a98842649eb48c3caa59d55eee48232d65bf14cc
Ayman ezz <aymanmae12@gmail.com>
2025-01-16 13:49:38 +0200

    sendMessages

--- frontend/src/components/messages/MessageInput.jsx
+++ frontend/src/components/messages/MessageInput.jsx
@@ -1,11 +1,29 @@
+import { useState } from "react";
 import { BiSend } from "react-icons/bi";
+import useSendMessage from "../../hooks/useSendMessage";
 const MessageInput = () => {
+  const [message, setMessage] = useState('')
+  const {sendMessage, loading} = useSendMessage();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if(!message) return
+    if (message.trim() !== '') {
+      await sendMessage({message});
+      setMessage('');
+    }
+  }
   return (
-    <div className="flex items-center gap-2 relative" >
-      <input type="text" placeholder="Type a message" className=" input input-bordered rounded-none focus:outline-none w-full pr-11"  />
-      <button type="submit" className="btn bg-transparent text-white border-none absolute inset-y-0 end-0 hover:bg-transparent" >
-        <BiSend className="w-5 h-5 hover:text-blue-500 " />  </button>
-    </div>
+    <form className="px-4 my-3" onSubmit={handleSubmit}>
+      <div className=" w-full relative" >
+        <input type="text" 
+          value={message}
+          onChange={(e)=> setMessage(e.target.value)}
+          placeholder="Type a message" className=" border text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white"  />
+        <button type="submit" className=" absolute inset-y-0 end-0 flex items-center pr-3" >
+          {loading ? <span className="loading loading-spinner"></span> : <BiSend className="w-6 h-6 text-white hover:text-blue-500 " />} 
+        </button>
+      </div>
+    </form>
   );
 };
 
--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,16 +1,23 @@
+import useGetMessages from "../../hooks/useGetMessages"
+import MessageSkeleton from "../skeletons/MessageSkeleton";
 import Message from "./Message"
 
 const Messages = () => {
+  const {messages,loading} = useGetMessages();  
   return (
-    <div className="px-4 flex-1 overflow-auto scrollbar-thin scrollbar-thumb-gray-500 scrollbar-track-transparent" >
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
+    <div className="px-4 flex-1 overflow-auto">
+      {!loading && messages.length >0 &&
+        messages.map((message) => <Message key={message?._id} message={message} /> ) 
+      }
+      { !loading && messages.length === 0 && (<p className="text-center text-white">No messages yet</p>) }
+      {loading && [...Array(4)].map((_, idx) => <MessageSkeleton key={idx} />)}
     </div>
   )
 }
 
 export default Messages
+
+
+{/* <div className="px-4 flex-1 overflow-auto scrollbar-thin scrollbar-thumb-gray-500 scrollbar-track-transparent" >
+      
+      </div> */}
